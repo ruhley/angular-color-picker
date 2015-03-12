e8327aebf375770820288744018c21cb7b9ff9a9
Ruhley <timothy.ruhle@gmail.com>
2015-03-12 16:23:26 +1000

    Updating examples

--- examples/app.js
+++ examples/app.js
@@ -6,4 +6,7 @@ app = angular
 		$scope.swatchPosOptions = [{label: 'Left', value: 'left'}, {label: 'Right', value: 'right'}];
 		$scope.posOptions = [{label: 'Bottom Left', value: 'bottom left'}, {label: 'Top Left', value: 'top left'}, {label: 'Bottom Right', value: 'bottom right'}, {label: 'Top Right', value: 'top right'}];
 		$scope.caseOptions = [{label: 'Upper Case', value: 'upper'}, {label: 'Lower Case', value: 'lower'}];
+        $scope.$watch('color', function(newValue, oldValue) {
+            console.log(newValue, oldValue);
+        });
 });
