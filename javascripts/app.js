4ee6d43fd8e7ded3481c59d30f62d4dee864d239
ruhley <timothy.ruhle@gmail.com>
2015-05-28 13:01:52 +1000

    Adding in hex8 option

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,7 @@
 angular
 	.module('app', ['color.picker'])
 	.controller('AppCtrl', function($scope) {
-		$scope.formatOptions = [{label: 'HSL', value: 'hsl'}, {label: 'HSV', value: 'hsv'}, {label: 'RGB', value: 'rgb'}, {label: 'HEX', value: 'hex'}];
+		$scope.formatOptions = [{label: 'HSL', value: 'hsl'}, {label: 'HSV', value: 'hsv'}, {label: 'RGB', value: 'rgb'}, {label: 'HEX', value: 'hex'}, {label: 'HEX8', value: 'hex8'}];
 		$scope.boolOptions = [{label: 'Yes', value: true}, {label: 'No', value: false}];
 		$scope.swatchPosOptions = [{label: 'Left', value: 'left'}, {label: 'Right', value: 'right'}];
 		$scope.posOptions = [{label: 'Bottom Left', value: 'bottom left'}, {label: 'Top Left', value: 'top left'}, {label: 'Bottom Right', value: 'bottom right'}, {label: 'Top Right', value: 'top right'}];
