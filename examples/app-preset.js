e17bf0219afbba398657918c48670b7dec4ae7b6
ruhley <timothy.ruhle@gmail.com>
2015-07-24 09:06:21 +1000

    Fixing #18 - Alpha value not working on initialise value

---
+++ examples/app-preset.js
@@ -0,0 +1,13 @@
+angular
+    .module('app', ['color.picker'])
+    .controller('AppCtrl', function($scope) {
+        $scope.formatOptions = [{label: 'HSL', value: 'hsl'}, {label: 'HSV', value: 'hsv'}, {label: 'RGB', value: 'rgb'}, {label: 'HEX', value: 'hex'}, {label: 'HEX8', value: 'hex8'}];
+        $scope.boolOptions = [{label: 'Yes', value: true}, {label: 'No', value: false}];
+        $scope.swatchPosOptions = [{label: 'Left', value: 'left'}, {label: 'Right', value: 'right'}];
+        $scope.posOptions = [{label: 'Bottom Left', value: 'bottom left'}, {label: 'Top Left', value: 'top left'}, {label: 'Bottom Right', value: 'bottom right'}, {label: 'Top Right', value: 'top right'}];
+        $scope.caseOptions = [{label: 'Upper Case', value: 'upper'}, {label: 'Lower Case', value: 'lower'}];
+        $scope.color = 'rgba(50, 75, 150, 0.4)';
+        $scope.$watch('color', function(newValue, oldValue) {
+            console.log(newValue, oldValue);
+        });
+    });
