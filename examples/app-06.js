a4515c2c527f59f3bd78d1b9ead4868e8fb529e9
Timothy Ruhle <timothy.ruhle@gmail.com>
2016-04-27 13:09:18 +1000

    Bug #65 non-assignable alpha when switching to hex

---
+++ examples/app-06.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', ['ui.bootstrap', 'color.picker']);
+
+app.controller('AppCtrl', function () {
+  var modal = this;
+
+  // Tile model properties, coming from the API
+  modal.edit = {
+    style: {
+      color: 'rgb(255, 255, 255)'
+    }
+  };
+
+  // Default (selected) colour format
+  modal.colorFormatFg = 'hex';
+
+  // Available colour formats (dropdown options)
+  modal.colorFormats = ['hex', 'rgb', 'hsl'];
+
+});
