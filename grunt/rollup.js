d44892eb33dad9770dc72207ffe7e35db52b2ca1
Timothy Ruhle <timothy.ruhle@gmail.com>
2016-07-11 16:31:31 +1000

    Bumping to v2.0.0

--- grunt/rollup.js
+++ grunt/rollup.js
@@ -2,6 +2,18 @@ var rollup = require('rollup');
 var babel = require('rollup-plugin-babel');
 var es2015Rollup = require('babel-preset-es2015-rollup');
 var uglify = require('rollup-plugin-uglify');
+var moment = require('moment');
+var pjson = require('../package.json');
+
+var today = moment();
+
+var writeOptions = {
+    dest: 'dist/angularjs-color-picker.js',
+    format: 'umd',
+    moduleName: 'AngularjsColorPicker',
+    sourceMap: false,
+    banner: '/*!\n * ' + pjson.name + ' v' + pjson.version + '\n * https://github.com/ruhley/angular-color-picker/\n *\n * Copyright ' + today.format('YYYY') + ' ruhley\n *\n * ' + today.format('YYYY-MM-DD HH:mm:ss') + '\n *\n */\n'
+};
 
 rollup.rollup({
     entry: 'src/scripts/module.js',
@@ -13,12 +25,7 @@ rollup.rollup({
     ]
 }).then(function(response) {
     try {
-        response.write({
-            dest: 'dist/angularjs-color-picker.js',
-            format: 'umd',
-            moduleName: 'AngularjsColorPicker',
-            sourceMap: false,
-        }).then(function() {
+        response.write(writeOptions).then(function() {
             console.log('File created');
         }, function() {
             console.log(arguments);
@@ -38,16 +45,23 @@ rollup.rollup({
             exclude: 'node_modules/**',
             presets: [ es2015Rollup ]
         }),
-        uglify(),
+        uglify({
+            output: {
+                comments: function(node, comment) {
+                    var text = comment.value;
+                    var type = comment.type;
+                    if (type == 'comment2') {
+                        // multiline comment
+                        return /!/i.test(text);
+                    }
+                }
+            }
+        }),
     ]
 }).then(function(response) {
     try {
-        response.write({
-            dest: 'dist/angularjs-color-picker.min.js',
-            format: 'umd',
-            moduleName: 'AngularjsColorPicker',
-            sourceMap: false,
-        }).then(function() {
+        writeOptions.dest = writeOptions.dest.replace('.js', '.min.js');
+        response.write(writeOptions).then(function() {
             console.log('File created');
         }, function() {
             console.log(arguments);
